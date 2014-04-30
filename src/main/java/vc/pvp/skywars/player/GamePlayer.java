bb6478cae28acd4bf85d8157cefb86d52d1630df
johandre <johannes.seldevall@telia.com>
2014-04-30 13:49:29 +0200

    Update GamePlayer.java

--- src/main/java/vc/pvp/skywars/player/GamePlayer.java
+++ src/main/java/vc/pvp/skywars/player/GamePlayer.java
@@ -27,7 +27,7 @@ public class GamePlayer {
     public GamePlayer(Player bukkitPlayer) {
         this.bukkitPlayer = bukkitPlayer;
         this.playerName = bukkitPlayer.getName();
-        this.UUID = playerName.getUUID();
+        this.UUID = playerName.getUniqueId();
 
         DataStorage.get().loadPlayer(this);
     }
@@ -35,7 +35,7 @@ public class GamePlayer {
     public GamePlayer(String playerName) {
         this.bukkitPlayer = null;
         this.playerName = playerName;
-        this.UUID = playerName.getUUID();
+        this.UUID = playerName.getUniqueId();
         DataStorage.get().loadPlayer(this);
     }
 
